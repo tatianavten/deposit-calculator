6890ac3fd1b8dea43166630083020ad0245adfb3
Tatiana Ten <tatiana.ten@iress.com>
2025-07-04 16:51:47 +1000

    chore: initial setup for cypress e2e

--- apps/calculator-ui-e2e/src/e2e/app.cy.ts
+++ apps/calculator-ui-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,8 @@
-import { getGreeting } from '../support/app.po';
+describe("@deposit-calculator/calculator-ui-e2e", () => {
+    beforeEach(() => cy.visit("/"));
 
-describe('@deposit-calculator/calculator-ui-e2e', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
-  });
+    it("should display welcome message", () => {
+        // Custom command example, see `../support/commands.ts` file
+        cy.contains("Term Deposit Calculator");
+    });
 });
