4956880dc5b193a3af059b5d491217f852844fcb
Tatiana Ten <tatiana.ten@iress.com>
2025-07-06 12:13:27 +1000

    chore: cleanup + test mock of dependency

--- apps/deposit-calculator-ui/src/app/app.spec.tsx
+++ apps/deposit-calculator-ui/src/app/app.spec.tsx
@@ -1,46 +1,36 @@
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { calculations } from '@deposit-calculator/calculations';
 
 import App from './app';
 
+jest.mock('@deposit-calculator/calculations', () => ({
+  calculations: jest.fn(() => 'Mocked calculations result'),
+}));
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getAllByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { getAllByText } = render(<App />);
     expect(
-      getAllByText(
-        new RegExp('Welcome @deposit-calculator/deposit-calculator', 'gi')
-      ).length > 0
+      getAllByText(new RegExp('Deposit calculator', 'gi')).length > 0
     ).toBeTruthy();
   });
 
   it('should render the header', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { getByText } = render(<App />);
     expect(getByText('Deposit Calculator')).toBeTruthy();
   });
 
+  it('should call calculations function', () => {
+    render(<App />);
+    expect(calculations).toHaveBeenCalled();
+  });
+
   it('should pass a11y checks', async () => {
-    const { container } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    const { container } = render(<App />);
     const results = await import('jest-axe').then(({ axe }) => axe(container));
     expect(results).toHaveNoViolations();
   });
--- apps/deposit-calculator-ui/src/app/app.tsx
+++ apps/deposit-calculator-ui/src/app/app.tsx
@@ -1,8 +1,11 @@
+import { calculations } from '@deposit-calculator/calculations';
+
 export function App() {
   return (
     <main>
       <h1>Deposit Calculator</h1>
       <p>Calculator to be added soon</p>
+      <p>{calculations()}</p>
     </main>
   );
 }
